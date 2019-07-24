b43767b3c9786d0953fbdc1de494a888a0ff61ce
Kelly Vaughn <kelly@thetaproom.com>
2019-07-24 14:42:29 -0400

    shop context update

--- src/components/shopcontext.js
+++ src/components/shopcontext.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Client from 'shopify-buy'
 
 const client = Client.buildClient({
-  domain: `${process.env.SHOPIFY_STORE_NAME}.myshopify.com`,
-  storefrontAccessToken: process.env.SHOPIFY_SECRET,
+  domain: `${process.env.GATSBY_SHOPIFY_STORE_NAME}.myshopify.com`,
+  storefrontAccessToken: process.env.GATSBY_SHOPIFY_SECRET,
 })
 
 export const defaultStoreContext = {
