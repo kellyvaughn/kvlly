321f34590e7264c983dbe3669bc87bb195e50abd
Kelly <kelly@thetaproom.com>
2021-02-07 09:31:29 -0500

    update website

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,35 +8,48 @@
     <h3>Work</h3>
     <ul>
       <li>
-        <a href="https://thetaproom.com" target="_blank" rel="noopener">Founder & CEO, The Taproom</a>
+        <a href="https://thetaproom.com" target="_blank" rel="noopener"
+          >Founder & CEO, The Taproom</a
+        >
       </li>
       <li>
-        <a
-          href="https://startfreelancing.today"
-          target="_blank"
-          rel="noopener"
-        >Author, Start Freelancing Today</a>
+        <a href="https://startfreelancing.today" target="_blank" rel="noopener"
+          >Author, Start Freelancing Today</a
+        >
       </li>
       <li>
-        <a href="https://shopkvlly.com" target="_blank" rel="noopener">Founder, KVLLY (Merch Shop)</a>
+        <a href="https://ladybug.dev" target="_blank" rel="noopener"
+          >Co-host, Ladybug Podcast</a
+        >
       </li>
       <li>
-        <a href="https://ladybug.dev" target="_blank" rel="noopener">Co-host, Ladybug Podcast</a>
-      </li>
-      <li>
-        <a href="https://commercetea.com" target="_blank" rel="noopener">Co-host, Commerce Tea</a>
+        <a href="https://commercetea.com" target="_blank" rel="noopener"
+          >Co-host, Commerce Tea</a
+        >
       </li>
     </ul>
     <h3>Social</h3>
     <ul>
       <li>
-        <a href="https://twitter.com/kvlly" target="_blank" rel="noopener">Twitter</a>
+        <a href="https://twitter.com/kvlly" target="_blank" rel="noopener"
+          >Twitter</a
+        >
       </li>
       <li>
-        <a href="https://instagram.com/taproomkelly" target="_blank" rel="noopener">Instagram</a>
+        <a
+          href="https://instagram.com/taproomkelly"
+          target="_blank"
+          rel="noopener"
+          >Instagram</a
+        >
       </li>
       <li>
-        <a href="https://linkedin.com/in/kellyvaughn" target="_blank" rel="noopener">LinkedIn</a>
+        <a
+          href="https://linkedin.com/in/kellyvaughn"
+          target="_blank"
+          rel="noopener"
+          >LinkedIn</a
+        >
       </li>
     </ul>
     <h3>Contact</h3>
@@ -45,43 +58,47 @@
         <a href="mailto:kelly@hey.com">kelly@hey.com</a>
       </li>
     </ul>
-    <h3>Commerce/Agency Consulting</h3>
+    <h3>Commerce/Agency/Freelancing Consulting</h3>
     <ul>
       <li>
-        <a href="https://calendly.com/taproom-kelly/consulting">30 minutes ($150)</a>
+        <a href="https://calendly.com/taproom-kelly/consulting"
+          >30 minutes ($200)</a
+        >
       </li>
       <li>
-        <a href="https://calendly.com/taproom-kelly/consulting-1-hour">1 hour ($300)</a>
+        <a href="https://calendly.com/taproom-kelly/consulting-1-hour"
+          >1 hour ($400)</a
+        >
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld"
-};
+  export default {
+    name: 'HelloWorld',
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  margin: 10px 0;
-}
-a {
-  color: #14668b;
-  text-decoration: none;
-}
-a:hover {
-  color: #093b52;
-  text-decoration: underline;
-}
+  li {
+    margin: 10px 0;
+  }
+  a {
+    color: #14668b;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #093b52;
+    text-decoration: underline;
+  }
 </style>
