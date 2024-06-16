acc4c8103cc9e483a8bcb08bc40f94d089c12f03
Kelly Vaughn <hello@kvlly.com>
2024-06-16 07:49:04 -0400

    remove example from url definition

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://kvlly.com',
     integrations: [
         mdx(),
         sitemap(),
